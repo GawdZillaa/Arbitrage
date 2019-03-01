44a89f34baa2e6a44784842961f720871bc1608b
Detrich Utti <Detrichutti@gmail.com>
2019-02-28 20:47:30 -0500

    Update footer.jsx

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 export default () => {
   return (
     <footer className="mt-5 p-4 footer">
-      Copyright &copy; {new Date().getFullYear()} Infosys DX/Indianapolis<br/>
       <Link className="open" style = {{color:'white'}} to="/contactus">
         contact us
       </Link> &nbsp;|&nbsp;
